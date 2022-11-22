e8aa91781774bcffec64c3b4a5094725947a71ea
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-11-22 16:21:07 +0200

    Add admins and test models

--- backend/board/admin.py
+++ backend/board/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.site_header = "Bulletin Board System"
+admin.site.site_title = "Bulletin Board System"
+
 
 # Register your models here.
+admin.site.register(User)
+
+# Topic, Board, Thread, Post, Reply
+admin.site.register(Board)
+admin.site.register(Thread)
+admin.site.register(Post)
