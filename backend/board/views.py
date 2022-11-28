2fb48068b7dd6be0ae9fbc51b8f8aafd4b3c1d49
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-11-28 04:27:33 +0200

    Add topic's viewset and fix bugs

--- backend/board/views.py
+++ backend/board/views.py
@@ -1,13 +1,18 @@
 import django_filters
+from django.contrib.auth import login
 from rest_framework import viewsets, status, generics
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.parsers import MultiPartParser
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.filters import OrderingFilter
 from knox.models import AuthToken
-from .models import User, Board, Thread, Post
+from .models import User, Topic, Board, Thread, Post
 from .serializers import (
     RegisterSerializer,
     LoginSerializer,
     UserSerializer,
+    TopicSerializer,
     BoardSerializer,
     ThreadSerializer,
     PostSerializer,
@@ -32,14 +37,14 @@ class RegisterAPI(generics.GenericAPIView):
 
 
 class LoginAPI(generics.GenericAPIView):
+    permission_classes = [AllowAny]
     serializer_class = LoginSerializer
 
     def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(
-            data=request.data, context={"request": request}
-        )
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data
+        login(request, user)
         return Response(
             {
                 "user": UserSerializer(
@@ -55,12 +60,47 @@ class UserView(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = User.objects.all()
+    pagination_class = None
+
+
+class TopicView(viewsets.ModelViewSet):
+    serializer_class = TopicSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Topic.objects.all()
+    pagination_class = None
+
+    def create(self, request):
+        post_data = request.data
+        if request.user.is_administrator == True and request.user.is_banned == False:
+            serializer = self.get_serializer(data=post_data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED, headers=headers
+            )
+        else:
+            raise PermissionDenied(
+                {"error": "You are no authorized to create a topic!"}
+            )
+
+    def destroy(self, request, *args, **kwargs):
+        if request.user.is_administrator == True and request.user.is_banned == False:
+            instance = self.get_object()
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise PermissionDenied(
+                {"error": "You are not authorized to delete a topic!"}
+            )
 
 
 class BoardView(viewsets.ModelViewSet):
     serializer_class = BoardSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Board.objects.all()
+    parser_classes = (MultiPartParser,)
+    pagination_class = None
 
     def create(self, request):
         post_data = request.data.dict()
@@ -85,11 +125,12 @@ class BoardView(viewsets.ModelViewSet):
 class ThreadView(viewsets.ModelViewSet):
     serializer_class = ThreadSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    queryset = Thread.objects.all().order_by("-is_sticky", "-created_at")
+    queryset = Thread.objects.all()
     filter_backends = [
         django_filters.rest_framework.DjangoFilterBackend,
     ]
-    ordering = ["-is_sticky", "created_at"]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filter_fields = ("board__id",)
 
 
 class PostView(viewsets.ModelViewSet):
@@ -98,5 +139,10 @@ class PostView(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by("-created_at")
     filter_backends = [
         django_filters.rest_framework.DjangoFilterBackend,
+        OrderingFilter,
     ]
+    filter_fields = (
+        "thread__id",
+        "poster_id",
+    )
     ordering = ["created_at"]
