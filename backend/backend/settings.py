aa30638b082a597510d7c8f737a2b72d6251ce83
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-12-06 20:09:37 +0200

    Delete secret keys

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,27 +10,35 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
+import os
 import environ
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# working with postgresql
-env = environ.Env(DEBUG=(bool, False))
-environ.Env.read_env()
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+
+# False if not in os.environ because of casting above
+DEBUG = env("DEBUG")
+
+# Raises Django's ImproperlyConfigured
+# exception if SECRET_KEY not in os.environ
+SECRET_KEY = env("SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xj!y7x@5c4p&*0!vws)rtofv7q4gf@9rz2+8t+kecso&l7hqqm"
-# Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
-# SECRET_KEY = env("SECRET_KEY")
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = env("DEBUG")
+# DEBUG = True
+
+# To test 404 page
 # DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
